b87814e84928d89f22970a91aa20e16d4bc2fda7
f1v3-dev <f1v3@kakao.com>
2023-09-02 13:11:02 +0900

    Checkstyle & Code style 적용

--- src/main/java/com/nhnacademy/mart/Counter.java
+++ src/main/java/com/nhnacademy/mart/Counter.java
@@ -5,6 +5,12 @@ package com.nhnacademy.mart;
  */
 public class Counter {
 
+    /**
+     * 고객이 가진 돈에서 총 액을 뺀 후 그 값을 리턴하는 방식을 사용
+     * 고객이 가진 돈보다 총 액이 더 크다면 예외 처리를 하였습니다.
+     *
+     */
+
     public int pay(int customerMoney, int total) {
 
         int money = customerMoney - total;
